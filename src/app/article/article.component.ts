027a0793210836c68ff1e3da1032e963f932692f
factorielle <dialobe110@gmail.com>
2023-11-22 13:52:20 +0000

    ajout article

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -9,6 +9,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ArticleComponent implements OnInit {
 
+  // ajout article variable
+  photo:string='';
+  titre:string='';
+  texte:string='';
+
   articles: any;
   userId: any;
   articlesByUser: any[] = [];
@@ -16,6 +21,9 @@ export class ArticleComponent implements OnInit {
   idUserConnect: any;
   search = "";
   searchResult: any[] = [];
+
+  userArticle:any[]=[]
+  userArticleRecup:any;
 
   constructor(private articleService: ArticleService, private route: ActivatedRoute, private router:Router) { }
 
@@ -27,6 +35,15 @@ export class ArticleComponent implements OnInit {
       // this.searchResult=reponse;
       console.log(this.articles)
     })
+
+    if (!localStorage.getItem('articles')) {
+      localStorage.setItem('articles', JSON.stringify(this.userArticle));
+      
+    }
+    this.userArticleRecup=JSON.parse(localStorage.getItem('articles')|| '[]');
+    console.log(this.userArticleRecup)
+    this.userArticleRecup.push(this.articlesByUser)
+    
     // console.log('articles', this.articles)
     // this.userConnect = this.articles.find((element: any) => element.userId == this.idUserConnect);
   }
@@ -59,5 +76,18 @@ export class ArticleComponent implements OnInit {
     this.router.navigate(['login']);
     localStorage.removeItem('userOnline');
 
+  }
+
+  addArticle(){
+    let article={
+      userId:this.idUserConnect,
+      id:this.userArticleRecup.length +1,
+      title:this.titre,
+      body:this.texte
+      
+    }
+    this.userArticleRecup[0].push(article)
+    localStorage.setItem('articles', JSON.stringify(this.userArticle))
+    console.log(this.userArticleRecup)
   }
 }
