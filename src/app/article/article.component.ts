1870840f6d78b57fe7a83e44588f08f546ba9897
saloum45 <saloumfall45@gmail.com>
2023-11-22 12:28:02 +0000

    search well done

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -10,15 +10,27 @@ export class ArticleComponent implements OnInit{
 
   articles:any;
   userId:any;
+  search="";
+  searchResult:any[]=[];
 
 
   constructor(private articleService:ArticleService){}
   ngOnInit(){
     this.articleService.getArticle().subscribe((reponse:any) => {
       this.articles=reponse;
+      this.searchResult=reponse;
       console.log(this.articles)
-      
-    })
+
+    });
+  }
+
+  getSearch(){
+    this.searchResult=[];
+      this.articles.forEach((element:any) => {
+        if (element.title.toLowerCase().includes(this.search.toLowerCase())) {
+          this.searchResult.push(element);
+        }
+      });
   }
 
 }
